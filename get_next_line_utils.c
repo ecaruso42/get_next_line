7c1a698bdb63d3898425a58a69d5fbd1e328f26a
Eugenio Caruso <ecaruso@student.42roma.it>
2023-02-16 17:34:06 +0100

    hello

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 16:03:42 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/02/16 16:45:36 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/02/16 17:33:49 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,49 @@ size_t	ft_strlen(const char *str)
 		i++;
 	}
 	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *backup, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!backup)
+	{
+		backup = (char *)malloc(1 * sizeof(char));
+		backup[0] = '\0';
+	}
+	if (!backup || !buff)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (backup)
+		while (backup[++i] != '\0')
+			str[i] = backup[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(backup) + ft_strlen(buff)] = '\0';
+	free(backup);
+	return (str);
 }
 
 char	*ft_get_line(char *backup)
